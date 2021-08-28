dd52450b83a6ff51b9a9a450211baad82f28bb98
HJPyo <bunker12383@gmail.com>
2021-08-29 00:08:07 +0900

    Make option modal and random sequence
    EZ

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 'use strict'
 
+/* make random sequence array */
+
+var myModal = new bootstrap.Modal(document.getElementById('modal'), {});
+myModal.toggle();
+var seqArray = [];
+
+function start() {
+	var formData = option.txt.value;
+	if (formData * 0 === 0 && formData != "") {
+		if (3 <= formData && formData <= 10000) {
+			myModal.toggle();
+			for(var i = 0; i < formData; i++) seqArray.push(i + 1);
+			for(var i = 0; i < formData; i++){
+				var rand = Math.floor(Math.random() * formData);
+				var tmp = seqArray[i];
+				seqArray[i] = seqArray[rand];
+				seqArray[rand] = tmp;
+			}
+			console.log(seqArray);
+		} else {
+			document.getElementById('lb1').style.display = 'none';
+			document.getElementById('lb2').style.display = 'block';
+		}
+	} else {
+		document.getElementById('lb1').style.display = 'block';
+		document.getElementById('lb2').style.display = 'none';
+	}
+}
+
+/* make canvas */
+
 const field = document.querySelector('#field');
 const myCanvas = document.getElementById('myCanvas');
 const ctx = myCanvas.getContext('2d');
@@ -8,21 +39,23 @@ ctx.canvas.height = field.offsetHeight;
 
 class cannonBall {
 	constructor() {
-		this.x = myCanvas.width / 2;
-		this.y = myCanvas.height / 2;
-		this.scale = 30;
+		this.x = 0;
+		this.y = 0;
+		this.scale = 20;
 	}
 	draw() {
-		var radius = myCanvas.width / this.scale;
-		var gx = this.x - radius;
-		var gy = this.y - radius;
-		var gradient = ctx.createRadialGradient(gx, gy, radius / 2, this.x, this.y, radius * 2);
+		var nx = this.x + (myCanvas.width / 2);
+		var ny = this.y + (myCanvas.height / 2) - 200 + (600 / this.scale);
+		var radius = myCanvas.height / this.scale;
+		var gx = nx - radius;
+		var gy = ny - radius;
+		var gradient = ctx.createRadialGradient(gx, gy, radius / 2, nx, ny, radius * 2);
 		gradient.addColorStop(0, '#03ff00');
 		gradient.addColorStop(1, '#127909');
 		ctx.fillStyle = gradient;
 		ctx.strokeStyle = 'rgba(0,0,0,0)';
 		ctx.beginPath();
-		ctx.arc(this.x, this.y, radius, 0, Math.PI * 2);
+		ctx.arc(nx, ny, radius, 0, Math.PI * 2);
 		ctx.fill();
 		ctx.stroke();
 	}
@@ -38,7 +71,7 @@ function loop() {
 	ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
 
 	ball.draw();
-	if(ball.scale > 8){
+	if (ball.scale > 5) {
 		ball.scale -= 0.5;
 	}
 	time++;
